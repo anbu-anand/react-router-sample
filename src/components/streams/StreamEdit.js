52792509cf179568d81ff132f53b30b6e0b04973
Anbu Anand <anbarasan.anand@yahoo.com>
2020-12-31 11:34:29 +0530

    reusable createStream Componenet

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { Component } from 'react'
+import { connect } from "react-redux";
+import { fetchStream } from '../../actions';
+export class StreamEdit extends Component {
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
-};
+    if(!this.props.stream){
+      return <div>Loading...</div>
+    }
 
-export default StreamEdit;
+    return (
+      <div>
+        {this.props.stream.title}
+        
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id],
+  }
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
